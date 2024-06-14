c252a7c1616f253c7a74b1868b3a83679081d2b0
mjclaypool <mallory.claypool@gmail.com>
2024-06-14 13:51:05 -0400

    Added timer for removing places to visit
    - Defined timeout function to remove places after 3 seconds with a useEffect hook.
    -Added a useCallback hook to avoid infinitely rerendering the function included in the useEffect hook's dependency array.

--- 11_Side-Effects-PlacePicker/src/components/DeleteConfirmation.jsx
+++ 11_Side-Effects-PlacePicker/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, 3000); // first arg (function) executes when second arg (time in ms) expires
+
+    return () => {
+      clearTimeout(timer);
+    } //clean-up function
+  }, [onConfirm]);
+
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
