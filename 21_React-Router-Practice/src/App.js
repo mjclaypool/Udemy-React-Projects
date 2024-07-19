ef44420cd935ef410a9667f77ec0ef9665a13c56
mjclaypool <mallory.claypool@gmail.com>
2024-07-19 10:50:46 -0400

    Imported react-router-dom into a simple SPA

---
+++ 21_React-Router-Practice/src/App.js
@@ -0,0 +1,25 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/Home.js';
+import ProductsPage from './pages/Products.js';
+import ProductDetailPage from './pages/ProductDetail.js';
+import RootLayout from './pages/Root.js';
+import ErrorPage from './pages/Error.js';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/products', element: <ProductsPage />},
+      { path: '/products/:productId', element: <ProductDetailPage /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
